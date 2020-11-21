7d9556553f8c61dcac43475586d5c4a9f890119e
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-11-21 15:30:08 -0600

    added api collection data from firebase, history, library, likedvideos, subs, watchlater, yourvideos

--- src/components/LikedVideos.js
+++ src/components/LikedVideos.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import VideoCard from '../VideoCard';
+import db from '../firebase';
 
 function LikedVideos() {
+  const [videos, setVideos] = useState([]);
+  // lifecycle method, componentDidUpdate is every single lifestyles method has been replace by useEffect method.
+  useEffect(() => {
+    // this is going to the collection from the Firestore that we created in the database,
+    // and it take a live snapshot of what the collection look like.
+    // and set the videos in our state to the following.
+    db.collection('likedvideos').onSnapshot(snapshot => (
+      setVideos(snapshot.docs.map((doc) => doc.data()))
+    ))
+    // if you dont include any dependencies in the array[],
+    // this code will fire once when the component load and whenever videos changes
+  }, [])
   return (
     <div>
-      <h1>LikedVideos</h1>
+        {videos.map(({id, channel, channelImage, image, timestamp, title, views}) => (
+          <VideoCard 
+            key={id}
+            channel={channel}
+            channelImage={channelImage}
+            image={image}
+            timestamp={timestamp + ' ' + "days ago"} 
+            title={title}
+            views={views + "K" + ' ' + "views"}
+          />
+        ))}
     </div>
   )
 }
 
-export default LikedVideos
+export default LikedVideos;
--- src/components/WatchLater.js
+++ src/components/WatchLater.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import VideoCard from '../VideoCard';
+import db from '../firebase';
 
 function WatchLater() {
+  const [videos, setVideos] = useState([]);
+  // lifecycle method, componentDidUpdate is every single lifestyles method has been replace by useEffect method.
+  useEffect(() => {
+    // this is going to the collection from the Firestore that we created in the database,
+    // and it take a live snapshot of what the collection look like.
+    // and set the videos in our state to the following.
+    db.collection('watchlater').onSnapshot(snapshot => (
+      setVideos(snapshot.docs.map((doc) => doc.data()))
+    ))
+    // if you dont include any dependencies in the array[],
+    // this code will fire once when the component load and whenever videos changes
+  }, [])
   return (
     <div>
-      <h1>WatchLater</h1>
+        {videos.map(({id, channel, channelImage, image, timestamp, title, views}) => (
+          <VideoCard 
+            key={id}
+            channel={channel}
+            channelImage={channelImage}
+            image={image}
+            timestamp={timestamp + ' ' + "days ago"} 
+            title={title}
+            views={views + "K" + ' ' + "views"}
+          />
+        ))}
     </div>
   )
 }
 
-export default WatchLater
+export default WatchLater;
