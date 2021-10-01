b5461b196524808d3c24b54f27130e9eeed13ddc
esteloabell <manitobaaa@gmail.com>
2021-10-01 18:07:15 -0500

    Update Reports.js

--- src/components/pages/Reports.js
+++ src/components/pages/Reports.js
@@ -1,38 +1,120 @@
 import React from 'react';
 import '../../styles/Reports.scss';
 import ComingSoon from '../ComingSoon';
+import OptionOne from './options/OptionOne';
+import OptionTwo from './options/OptionTwo';
+import OptionThree from './options/OptionThree';
 
-export const Reports = () => {
+export const ShowmoreMain = () => {
   return (
-    <div className='reports'>
-      <ComingSoon />
+    <div className='showmore'>
+      <div className='showmore__container'>
+        <div className="showmore__item-container">
+          {/* <OptionOne /> */}
+          <div className="showmore__item">
+            <div className="showmore_-item-option-one">
+              <p>optionOne</p>
+            </div>
+          </div>
+        </div>
+        <div className="showmore__item-container">
+          {/* <OptionTwo /> */}
+          <div className="showmore__item">
+            <div className="showmore_-item-option-two">
+              <p>optionTwo</p>
+            </div>
 
+          </div>
+        </div>
+        <div className="showmore__item-container">
+          {/* <OptionThree /> */}
+          <div className="showmore__item">
+            <div className="showmore_-item-option-three">
+              <p>optionThree</p>
+            </div>
+          </div>
+        </div>
+        <div className="showmore__item-container">
+          {/* <OptionThree /> */}
+          <div className="showmore__item">
+            <div className="showmore_-item-option-three">
+              <p>optionThree</p>
+            </div>
+          </div>
+        </div>
+        <div className="showmore__item-container">
+          {/* <OptionThree /> */}
+          <div className="showmore__item">
+            <div className="showmore_-item-option-three">
+              <p>optionThree</p>
+            </div>
+          </div>
+        </div>
+        <div className="showmore__item-container">
+          {/* <OptionThree /> */}
+          <div className="showmore__item">
+            <div className="showmore_-item-option-three">
+              <p>optionThree</p>
+            </div>
+          </div>
+        </div>
+        <div className="showmore__item-container">
+          {/* <OptionThree /> */}
+          <div className="showmore__item">
+            <div className="showmore_-item-option-three">
+              <p>optionThree</p>
+            </div>
+          </div>
+        </div>
+        <div className="showmore__item-container">
+          {/* <OptionThree /> */}
+          <div className="showmore__item">
+            <div className="showmore_-item-option-three">
+              <p>optionThree</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="showmore__bottom">
+        <div className="showmore__bottom-container">
+          <div className="showmore__bottom-child">
+            helloo
+          </div>
+        </div>
+        <div className="showmore__bottom-container">
+          <div className="showmore__bottom-child">
+            helloo
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export const ReportsOne = () => {
-  return (
-    <div className='reports'>
-      <ComingSoon />
-    </div>
-  );
-};
+// export const OptionOne = () => {
+//   return (
+//     <div className='option-one'>
+//       {/* <ComingSoon /> */}
+//       music
+//     </div>
+//   );
+// };
 
-export const ReportsTwo = () => {
-  return (
-    <div className='reports'>
-      <ComingSoon />
+// export const OptionTwo = () => {
+//   return (
+//     <div className='option-two'>
+//       {/* <ComingSoon /> */}
+//       code
 
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
 
-export const ReportsThree = () => {
-  return (
-    <div className='reports'>
-      <ComingSoon />
-
-    </div>
-  );
-};
+// export const OptionThree = () => {
+//   return (
+//     <div className='option-three'>
+//       {/* <ComingSoon /> */}
+//       anime
+//     </div>
+//   );
+// };
