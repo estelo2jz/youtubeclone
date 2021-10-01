dbf134c259a3510f40a5bd0fbe34f25d83f3aad0
esteloabell <manitobaaa@gmail.com>
2021-10-01 08:44:43 -0500

    changed font-size to images and p tag

--- src/components/category/Trending.js
+++ src/components/category/Trending.js
@@ -39,6 +39,26 @@ function Trending() {
           <img src="https://youtube.com/img/trending/chips/fashion_80x80.png" alt="trending-logo" />
           <p>Fashion & Beauty</p>
         </div>
+        <div className="trending__category">
+          <img src="https://youtube.com/img/trending/chips/music_80x80.png" alt="trending-logo" />
+          <p>Music</p>
+        </div>
+        <div className="trending__category">
+          <img src="https://youtube.com/img/trending/chips/gaming_80x80.png" alt="trending-logo" />
+          <p>Gaming</p>
+        </div>
+        <div className="trending__category">
+          <img src="https://youtube.com/img/trending/chips/news_80x80.png" alt="trending-logo" />
+          <p>News</p>
+        </div>
+        <div className="trending__category">
+          <img src="https://youtube.com/img/trending/chips/movies_80x80.png" alt="trending-logo" />
+          <p>Movies</p>
+        </div>
+        <div className="trending__category">
+          <img src="https://youtube.com/img/trending/chips/fashion_80x80.png" alt="trending-logo" />
+          <p>Fashion & Beauty</p>
+        </div>
       </div>
       <div className="trending__video-container">
         <div className="trending__videos">
