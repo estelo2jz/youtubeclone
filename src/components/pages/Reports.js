a41c11e474e8bf15fa5a55f7f7d578f2f7eaa342
esteloabell <manitobaaa@gmail.com>
2021-10-01 21:15:23 -0500

    Update Reports.js

--- src/components/pages/Reports.js
+++ src/components/pages/Reports.js
@@ -5,15 +5,30 @@ import OptionOne from './options/OptionOne';
 import OptionTwo from './options/OptionTwo';
 import OptionThree from './options/OptionThree';
 
+
+import MusicBg from '../images/optionImage/music.jpg';
+import CodeBg from '../images/optionImage/codes.jfif';
+import AnimeBg from '../images/optionImage/anime.jfif';
+import AnimeSongBg from '../images/optionImage/animesong.jfif';
+import JDMBg from '../images/optionImage/jdm.jfif';
+import ReactBg from '../images/optionImage/react.png';
+import NativeBg from '../images/optionImage/native.png';
+
 export const ShowmoreMain = () => {
   return (
     <div className='showmore'>
+      <div className='showmore__saved'>
+        Saved
+      </div>
       <div className='showmore__container'>
         <div className="showmore__item-container">
           {/* <OptionOne /> */}
           <div className="showmore__item">
             <div className="showmore_-item-option-one">
-              <p>optionOne</p>
+              <p>
+                <span>Music's</span>
+                <img src={MusicBg} alt="image" />
+              </p>
             </div>
           </div>
         </div>
@@ -21,7 +36,10 @@ export const ShowmoreMain = () => {
           {/* <OptionTwo /> */}
           <div className="showmore__item">
             <div className="showmore_-item-option-two">
-              <p>optionTwo</p>
+              <p>
+                <span>Code's</span>
+                <img src={CodeBg} alt="image" />
+              </p>
             </div>
 
           </div>
@@ -30,7 +48,24 @@ export const ShowmoreMain = () => {
           {/* <OptionThree /> */}
           <div className="showmore__item">
             <div className="showmore_-item-option-three">
-              <p>optionThree</p>
+              <p>
+                <span>Anime's</span>
+                <img src={AnimeBg} alt="image" />
+              </p>
+
+            </div>
+          </div>
+        </div>
+        <div className="showmore__item-container">
+          {/* <OptionThree /> */}
+          <div className="showmore__item">
+            {/* <img src={} alt="image"/> */}
+            <div className="showmore_-item-option-three">
+              <p>
+                <span>Anime song OP</span>
+                <img src={AnimeSongBg} alt="image" />
+              </p>
+
             </div>
           </div>
         </div>
@@ -38,7 +73,11 @@ export const ShowmoreMain = () => {
           {/* <OptionThree /> */}
           <div className="showmore__item">
             <div className="showmore_-item-option-three">
-              <p>optionThree</p>
+              <p>
+                <span>JDM cars</span>
+                <img src={JDMBg} alt="image" />
+              </p>
+
             </div>
           </div>
         </div>
@@ -46,7 +85,11 @@ export const ShowmoreMain = () => {
           {/* <OptionThree /> */}
           <div className="showmore__item">
             <div className="showmore_-item-option-three">
-              <p>optionThree</p>
+              <p>
+                <span>React</span>
+                <img src={ReactBg} alt="image" />
+              </p>
+
             </div>
           </div>
         </div>
@@ -54,38 +97,128 @@ export const ShowmoreMain = () => {
           {/* <OptionThree /> */}
           <div className="showmore__item">
             <div className="showmore_-item-option-three">
-              <p>optionThree</p>
-            </div>
-          </div>
-        </div>
-        <div className="showmore__item-container">
-          {/* <OptionThree /> */}
-          <div className="showmore__item">
-            <div className="showmore_-item-option-three">
-              <p>optionThree</p>
-            </div>
-          </div>
-        </div>
-        <div className="showmore__item-container">
-          {/* <OptionThree /> */}
-          <div className="showmore__item">
-            <div className="showmore_-item-option-three">
-              <p>optionThree</p>
+              <p>
+                <span>React Native</span>
+               <img src={NativeBg} alt="image" />
+              </p>
+
             </div>
           </div>
         </div>
       </div>
-      <div className="showmore__bottom">
-        <div className="showmore__bottom-container">
-          <div className="showmore__bottom-child">
-            helloo
+      <div className="showmore__bottom wrapper">
+        <div className="wrapper">
+          <div id="one">
+            <div className="showmore__bottom-left">
+              <i className="showmore__icon" class="fab fa-youtube" id="youtube-logo"></i>
+            </div>
+            <div className="showmore__bottom-right">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+          </div>
+          <div id="two">
+            <div className="showmore__bottom-left">
+              <i className="showmore__icon" class="fab fa-youtube" id="youtube-logo"></i>
+            </div>
+            <div className="showmore__bottom-right">
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="showmore__bottom-container">
-          <div className="showmore__bottom-child">
-            helloo
+        <div className="wrapper">
+          <div id="three">
+            <div className="showmore__bottom-left">
+              <i className="showmore__icon" class="fab fa-youtube" id="youtube-logo"></i>
+            </div>
+            <div className="showmore__bottom-right">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+          </div>
+          <div id="four">
+            <div className="showmore__bottom-left">
+              <i className="showmore__icon" class="fab fa-youtube" id="youtube-logo"></i>
+            </div>
+            <div className="showmore__bottom-right">
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
+        <div className="wrapper">
+          <div id="five">
+            <div className="showmore__bottom-left">
+              <i className="showmore__icon" class="fab fa-youtube" id="youtube-logo"></i>
+            </div>
+            <div className="showmore__bottom-right">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+          </div>
+          <div id="six">
+            <div className="showmore__bottom-left">
+              <i className="showmore__icon" class="fab fa-youtube" id="youtube-logo"></i>
+            </div>
+            <div className="showmore__bottom-right">
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="wrapper">
+          <div id="seven">
+            <div className="showmore__bottom-left">
+              <i className="showmore__icon" class="fab fa-youtube" id="youtube-logo"></i>
+            </div>
+            <div className="showmore__bottom-right">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+          </div>
+          <div id="eight">
+            <div className="showmore__bottom-left">
+              <i className="showmore__icon" class="fab fa-youtube" id="youtube-logo"></i>
+            </div>
+            <div className="showmore__bottom-right">
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div className="showmore__bottom-container">
+          <h2>Column 1</h2>
+          <p>Some text..</p>
+        </div> */}
+        {/* <div className="showmore__bottom-container">
+          <h2>Column 2</h2>
+          <p>Some text..</p>
+        </div> */}
       </div>
     </div>
   );
