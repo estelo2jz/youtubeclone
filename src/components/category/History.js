988131aa09b2dc80a09783fce239a3cf5eebfbee
esteloabell <manitobaaa@gmail.com>
2021-10-03 15:44:58 -0500

    Update History.js

--- src/components/category/History.js
+++ src/components/category/History.js
@@ -38,15 +38,83 @@ function History() {
     <div className="history__container">
       <div className="history__right-outer">
         <div className="history__right-container">
-          <div className="history__search">
-            <div>
-              <SearchIcon />
+          <div className="wrapper">
+            <div id="one">
+              <div className="history__type-container">
+                <div className="history__search">
+                  <div>
+                    <SearchIcon />
+                  </div>
+                  <div className="history__input">
+                    <input placeholder="Search watch history" />
+                  </div>
+                </div>
+                <div className="history__history-header">
+                  <p>History type</p>
+                </div>
+                <div className="history__input-container">
+                  <div className="history__watch-history-container">
+                    <div>
+                      <p>Watch history</p>
+                    </div>
+                    <div>
+                      <input type="radio" checked="checked" checked />
+                    </div>
+                  </div>
+                  <div className="history__community-container">
+                    <div>
+                      <p>Community</p>
+                    </div>
+                    <div>
+                      <input type="radio" checked="" unchecked />
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="history__input">
-              <input placeholder="Search watch history" />
+            <div id="two">
+              <div className="history__bottom-container">
+                <div className="history__delete-container">
+                  <icon>
+                    <DeleteIcon />
+                  </icon>
+                  <p>clear all watch history</p>
+                </div>
+                <div className="history__pause-container">
+                  <icon>
+                    <PauseCircleOutlineIcon />
+                  </icon>
+                  <p>pause all history</p>
+                </div>
+                <div className="history__manage-container">
+                  <icon>
+                    <SettingsIcon />
+                  </icon>
+                  <p>manage all history</p>
+                </div>
+                <div className="history__link-one">
+                  <Link to="#">Watch and search history</Link>
+                </div>
+                <div className="history__link-two">
+                  <Link to="#">Comments</Link>
+                </div>
+                <div className="history__link-three">
+                  <Link to="#">Live Chat</Link>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="history__type-container">
+
+
+          {/* <div className="history__type-container">
+            <div className="history__search">
+              <div>
+                <SearchIcon />
+              </div>
+              <div className="history__input">
+                <input placeholder="Search watch history" />
+              </div>
+            </div>
             <div className="history__history-header">
               <p>History type</p>
             </div>
@@ -97,7 +165,7 @@ function History() {
             <div  className="history__link-three">
               <Link to="#">Live Chat</Link>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
 
