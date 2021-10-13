4efcffcabaf49a48fdcd8a84c6ccd7e81cfae062
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-13 15:25:04 -0500

    Create SubscriptionDetails.js

---
+++ src/components/category/Details/CategoryDetails/SubscriptionDetails.js
@@ -0,0 +1,114 @@
+import React, { useContext, useState, useRef } from 'react'
+import { useParams } from 'react-router-dom'
+import {SubscriptionDataContext} from '../CategoryInventory/SubscriptionInventory';
+import { Link } from 'react-router-dom'
+
+// import DetailsThumb from '../../DetailsThumb';
+// import JapanContacts from './DetailsContacts/JapanContacts';
+
+import SearchIcon from '@material-ui/icons/Search';
+
+
+import './styles/main.scss';
+// import HeaderInventory from '../../Home/HeaderInventory';
+
+export default function SubscriptionDetails() {
+  const { id } = useParams();
+  const value = useContext(SubscriptionDataContext)
+  const [products] = value.products
+
+  const [index, setIndex] = useState(0)
+  const imgDiv = useRef();
+
+
+  const details = products.filter((product, index) => {
+    return product._id === id
+  })
+
+  return (
+    <>
+      {
+        details.map(product => (
+          <div className="details" key={product._id}>
+            {/* <div className="details__header">
+              <div className="details-header__fullname">
+                <p>{product.title}</p>
+              </div>
+              <div className="details__price">
+                <h3>{product.title}</h3>
+              </div>
+            </div> */}
+            <div className="box-details">
+              <div className="box-img">
+                <div className="box-img-container" 
+                  style={{ backgroundImage: `url(${product.images[index]})` }} ref={imgDiv}
+                />
+                <div>
+                  {/* <DetailsThumb images={product.images} setIndex={setIndex} /> */}
+                </div>
+              </div>
+              <div className="box-specs-container">
+                <p className="box-specs-items">
+                  <span></span>
+                  <span>
+                    {product.title}
+                  </span>
+                </p>
+                <p className="box-specs-items">
+                  <span></span>
+                  <span>
+                    {product.channel}
+                  </span>
+                </p>
+                <p className="box-specs-items">
+                  <span></span>
+                  <span>
+                    {product.channelImage}
+                  </span>
+                </p>
+                <p className="box-specs-items">
+                  <span></span>
+                  <span>
+                    {product.views}
+                  </span>
+                </p>
+                <p className="box-specs-items">
+                  <span></span>
+                  <span>
+                    {product.timestamp}
+                  </span>
+                </p>
+                <p className="box-specs-items">
+                  <span></span>
+                  <span>
+                    {product.icon}
+                  </span>
+                </p>
+                <p className="box-specs-items">
+                  <span></span>
+                  <span>
+                    {product.filter}
+                  </span>
+                </p>
+                <p className="box-specs-items">
+                  <span></span>
+                  <span>
+                    {product.desc}
+                  </span>
+                </p>
+                <p className="box-specs-items">
+                  <span></span>
+                  <span>
+                    {product.mazViews}
+                  </span>
+                </p>
+              </div>
+            </div>
+            {/* <JapanContacts />
+            <HeaderInventory /> */}
+          </div>
+        ))
+      }
+    </>
+  )
+}
