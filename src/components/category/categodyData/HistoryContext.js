bae3dbc0b3d3ca10755e00794383c78ad9bc8abc
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-13 15:25:45 -0500

    Update HistoryContext.js

--- src/components/category/categodyData/HistoryContext.js
+++ src/components/category/categodyData/HistoryContext.js
@@ -4,8 +4,8 @@ import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 export const HistoryData = [
   {
     title: "APEX LEGENDS │#VTuber最協決定戦S2 練習カスタム",
-    image: "https://i.ytimg.com/vi/LySyKUFp9PY/hqdefault_live.jpg?sqp=-oaymwEbCNIBEHZIVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDi47m8ItGwv9GEOjuqVHJnD4rM-Q",
     channel: "APEX Legends",
+    image: "https://i.ytimg.com/vi/LySyKUFp9PY/hqdefault_live.jpg?sqp=-oaymwEbCNIBEHZIVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDi47m8ItGwv9GEOjuqVHJnD4rM-Q",
     channalImage: "https://yt3.ggpht.com/ytc/AAUvwniYDSFbbomSJwdAmgyLsoxsoY9VCV4qY2KvQEfwdg=s48-c-k-c0xffffffff-no-rj-mo",
     views: "456",
     timestamp: "23",
