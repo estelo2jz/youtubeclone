f9ccec69f136d4824b00a0b3c6f3b04fd2dc89e8
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-14 12:58:33 -0500

    Update LibraryDetails.js

--- src/components/category/Details/CategoryDetails/LibraryDetails.js
+++ src/components/category/Details/CategoryDetails/LibraryDetails.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 import {LibraryDataContext} from '../CategoryInventory/LibraryInventory';
 import { Link } from 'react-router-dom'
 
-import VideoBio from './components/VideoBio';
+import VideoBio from './components/VideoComponents/LibraryBio';
 import RecommendVideos from './components/RecommendVideos';
 
 
