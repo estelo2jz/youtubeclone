b77493b888902a2e43e21377f78b1dde4da8d413
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-14 12:58:31 -0500

    Update HistoryDetails.js

--- src/components/category/Details/CategoryDetails/HistoryDetails.js
+++ src/components/category/Details/CategoryDetails/HistoryDetails.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 import {HistoryDataContext} from '../CategoryInventory/HistoryInventory';
 import { Link } from 'react-router-dom'
 
-import VideoBio from './components/VideoBio';
+import VideoBio from './components/VideoComponents/HistoryBio';
 import RecommendVideos from './components/RecommendVideos';
 
 import SearchIcon from '@material-ui/icons/Search';
