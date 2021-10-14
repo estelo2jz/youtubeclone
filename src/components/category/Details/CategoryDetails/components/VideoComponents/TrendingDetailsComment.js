8bc725f26155c022c512f3c308900d0f8fd1b6cb
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-14 12:58:39 -0500

    Create TrendingDetailsComment.js

---
+++ src/components/category/Details/CategoryDetails/components/VideoComponents/TrendingDetailsComment.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+import { FiThumbsUp, FiThumbsDown } from "react-icons/fi";
+import { AiFillCaretDown } from "react-icons/ai";
+
+import "../styles/detailsComment.scss";
+
+export default function DetailsComments({ comments, setIndex }) {
+  return (
+    <div className="details-comments">
+      <div className="details-comments__container">
+        {comments.map((item, index) => (
+          <div className="details-comments__inner">
+            <div className="details-comments__comment-number">
+              <div className="details-comments__number">
+                <p>{item.commentsNumber}</p>
+              </div>
+            </div>
+            <div className="details-comments__section">
+              <div className="details-comments__top-section">
+                <div className="details-comments__img">
+                  <img src={item.imgChannel} alt="" />
+                </div>
+                <div className="details-comments__profile">
+                  <div className="details-comments__user">
+                    <p>{item.user}</p>
+                    <p>{item.date}</p>
+                  </div>
+                  <div className="details-comments__commented">
+                    <p>{item.comments}</p>
+                  </div>
+                  <div className="details-comments__thumbs">
+                    <div className="details-comments__thumbs-up">
+                      <FiThumbsUp />
+                      <p>{item.thumbsUp}</p>
+                    </div>
+                    <div className="details-comments__thumbs-down">
+                      <FiThumbsDown />
+                      <p>{item.thumbsDown}</p>
+                    </div>
+                    <div className="details-comments__thumbs-reply">
+                      <p>REPLY</p>
+                    </div>
+                  </div>
+                  <div className="details-comments__replies">
+                    <p>
+                      <AiFillCaretDown />
+                    </p>
+                    <p>View {item.replies} replies</p>
+                  </div>
+                </div>
+              </div>
+              <div className="details-comments__bottom-section"></div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
