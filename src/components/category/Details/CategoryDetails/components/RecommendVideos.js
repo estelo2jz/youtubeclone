402b8ef5e72348863137e008e5f24fa9826088c2
esteloabell <manitobaaa@gmail.com>
2021-10-13 21:37:42 -0500

    Create RecommendVideos.js

---
+++ src/components/category/Details/CategoryDetails/components/RecommendVideos.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './styles/recVideos.scss';
+
+const RecommendVideos = () => {
+  return (
+    <div className="recommended">
+      <div className="recommended__container">
+        recommend videos
+      </div>
+      <div className="recommended__contents">
+        
+      </div>
+    </div>
+  )
+}
+
+export default RecommendVideos
