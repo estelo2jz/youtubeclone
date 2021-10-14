c0001da3a45f6215c6790936e65ab8215a7e932c
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-14 12:58:36 -0500

    Update TrendingDetails.js

--- src/components/category/Details/CategoryDetails/TrendingDetails.js
+++ src/components/category/Details/CategoryDetails/TrendingDetails.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 import {TrendingDataContext} from '../CategoryInventory/TrendingInventory';
 import { Link } from 'react-router-dom'
 
-import VideoBio from './components/VideoBio';
+import VideoBio from './components/VideoComponents/TrendingBio';
 import RecommendVideos from './components/RecommendVideos';
 
 import SearchIcon from '@material-ui/icons/Search';
