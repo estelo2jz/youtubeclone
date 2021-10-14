8920c288a9cfc52d35197d032a0af82e562840a0
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-14 11:57:27 -0500

    Update VideoBio.js

--- src/components/category/Details/CategoryDetails/components/VideoBio.js
+++ src/components/category/Details/CategoryDetails/components/VideoBio.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom'
 import { HomeDataContext } from '../../CategoryInventory/HomeInventory';
 import { Link } from 'react-router-dom'
 
+import Comments from './Comments';
+
 import SearchIcon from '@material-ui/icons/Search';
 import { FiThumbsUp, FiThumbsDown } from 'react-icons/fi';
 import { RiShareForwardLine, RiPlayListAddFill } from 'react-icons/ri';
@@ -131,6 +133,7 @@ export default function VideoBio() {
           </div>
         ))
       }
+      <Comments />
     </>
   )
 }
