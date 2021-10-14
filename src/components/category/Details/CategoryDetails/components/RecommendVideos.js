f805a4225232c0c7811fa29238808bec81b7bc26
esteloabell <manitobaaa@gmail.com>
2021-10-13 22:02:05 -0500

    Update RecommendVideos.js

--- src/components/category/Details/CategoryDetails/components/RecommendVideos.js
+++ src/components/category/Details/CategoryDetails/components/RecommendVideos.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import HomeData from '../../CategoryData/HomeData';
+
 import './styles/recVideos.scss';
 
 const RecommendVideos = () => {
@@ -7,9 +9,10 @@ const RecommendVideos = () => {
     <div className="recommended">
       <div className="recommended__container">
         recommend videos
+        <HomeData />
       </div>
       <div className="recommended__contents">
-        
+
       </div>
     </div>
   )
