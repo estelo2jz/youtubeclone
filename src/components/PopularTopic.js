2655118a151a2f810d6f714939d8c37a4370c1af
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-28 16:33:25 -0500

    Update PopularTopic.js

--- src/components/PopularTopic.js
+++ src/components/PopularTopic.js
@@ -1,172 +1,134 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './styles/PopularTopic.scss';
+import "./styles/PopularTopic.scss";
 
 function PopularTopic() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
+    
     <div className="popular-topic__container">
       <div className="popular-topic__box">
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Music
-            </label>
+            <label>Music</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Recently Uploaded
-            </label>
+            <label>Recently Uploaded</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Live
-            </label>
+            <label>Live</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Gaming
-            </label>
+            <label>Gaming</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Trailers
-            </label>
+            <label>Trailers</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Latest News
-            </label>
+            <label>Latest News</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Most Popular
-            </label>
+            <label>Most Popular</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Most Watched
-            </label>
+            <label>Most Watched</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Recently Watched
-            </label>
+            <label>Recently Watched</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Explore
-            </label>
+            <label>Explore</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Channels
-            </label>
+            <label>Channels</label>
           </Link>
         </div>
 
-
-
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Music
-            </label>
+            <label>Music</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Recently Uploaded
-            </label>
+            <label>Recently Uploaded</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Live
-            </label>
+            <label>Live</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Gaming
-            </label>
+            <label>Gaming</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Trailers
-            </label>
+            <label>Trailers</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Latest News
-            </label>
+            <label>Latest News</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Most Popular
-            </label>
+            <label>Most Popular</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Most Watched
-            </label>
+            <label>Most Watched</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Recently Watched
-            </label>
+            <label>Recently Watched</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Explore
-            </label>
+            <label>Explore</label>
           </Link>
         </div>
         <div className="popular-topic__item">
           <Link className="popular-topic__link" to="/">
-            <label>
-              Channels
-            </label>
+            <label>Channels</label>
           </Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default PopularTopic
+export default PopularTopic;
