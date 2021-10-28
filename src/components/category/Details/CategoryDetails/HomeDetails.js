10ea661402397c2f76659b58e97ba8b77535e627
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-28 16:32:49 -0500

    Update HomeDetails.js

--- src/components/category/Details/CategoryDetails/HomeDetails.js
+++ src/components/category/Details/CategoryDetails/HomeDetails.js
@@ -18,10 +18,6 @@ export default function HomeDetails() {
   const value = useContext(HomeDataContext)
   const [products] = value.products
 
-  const [index, setIndex] = useState(0)
-  const imgDiv = useRef();
-
-
   const details = products.filter((product, index) => {
     return product._id === id
   })
@@ -34,10 +30,7 @@ export default function HomeDetails() {
             <div className="box-details">
               <div className="box-img">
                 <div className="box-img-container">
-                  {/* <video loop controls autoPlay={true} muted >
-                   <source src={product.video} type="video/mp4"/>
-                  </video> */}
-                   <ReactPlayer width="100%" height="500px" controls="true" playing='true' url={product.video} />
+                   <ReactPlayer onEnded={true} width="100%" height="500px" controls="true" playing='true' url={product.video} />
                 </div>
                 <div className="box-img-info">
                   <div className="box-img-info-contents">
