6363b147e0e8a6fb7bff6fe19e750d5f534de841
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-29 13:09:01 -0500

    Create CarouselData.js

---
+++ src/components/category/Carousel/CarouselData.js
@@ -0,0 +1,47 @@
+export const data = [
+  {
+    title: "Music",
+    link: "",
+  },
+  {
+    title: "Live",
+    link: "",
+  },
+  {
+    title: "GAming",
+    link: "",
+  },
+  {
+    title: "Trailers",
+    link: "",
+  },
+  {
+    title: "Latest News",
+    link: "",
+  },
+  {
+    title: "Most Popular",
+    link: "",
+  },
+]
+
+export const sliderSettings = {
+  arrow: false,
+  slidesToShow: 3,
+  focusOnselect: false,
+  accessability: false,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+}
