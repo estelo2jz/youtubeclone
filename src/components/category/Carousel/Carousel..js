7d820828727487d6dbae81bfb8564b5a2ce330e9
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-29 13:08:59 -0500

    Create Carousel..js

---
+++ src/components/category/Carousel/Carousel..js
@@ -0,0 +1,15 @@
+import React, {useState}  from 'react';
+import {FaArrowCircleLeft, FaArrowCircleRight} from 'react-icons/fa';
+import {IconContext} from 'react-icons';
+import {data, sliderSettings} from './CarouselData';
+import {Row, Heading, Section, TextWrapper} from './grobalStyles';
+
+const Carousel = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Carousel
